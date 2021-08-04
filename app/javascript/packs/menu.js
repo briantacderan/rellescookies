9e9ff97073b619cba1948a4872510dcb2efd1b1b
Brian Tacderan <briantacderan@gmail.com>
2021-08-04 16:17:02 -0700

    Update menu functionality for original cookies

--- app/javascript/packs/menu.js
+++ app/javascript/packs/menu.js
@@ -2,4 +2,10 @@ document.querySelector("#original.cat-name").addEventListener("click", function(
   document.querySelector("img.seasonal").classList.add("hide");
   document.querySelector("#seasonal h1").classList.add("hide");
   document.querySelector("#seasonal h1").classList.add("hide");
+  document.querySelector("#category-background").classList.add("hide");
+  document.querySelector("#original").classList.add("show");
+  document.querySelector("#seasonal").classList.add("show");
+  document.querySelector("#menu-section").classList.add("move");
+  document.querySelector("h1#original.cat-name").classList.add("move");
+  document.querySelector("#bulletin-left.og").classList.add("show");
 });
